53a87472b4122dcaf98a99d132ee0784e2df4078
BeckySkel <beckyskelcher@hotmail.co.uk>
2022-08-04 07:15:21 +0000

    Add password creation and validation

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ USERS_SHEET = SHEET.worksheet('users')
 def validate_username_creation():
     """
     Prompts the user to choose a username. Checks if it already exists
-    and restarts if it does. Returns True if username available
+    and restarts if it does.
     """
     username = input("Username:\n")
     usernames = USERS_SHEET.col_values(3)[1:]
@@ -29,14 +29,50 @@ def validate_username_creation():
     if username in usernames:
         print("That username is already taken, please try again\n")
         validate_username_creation()
+    
+    return username
+
+
+def validate_password_creation():
+    """
+    Prompts the user to choose a password. Checks if it is at least 5
+    characters long and contains at least 1 uppercase character
+    """
+    print("""Password must meet the following criteria:
+    - at least 5 characters long
+    - contain at least 1 upppercase and 1 lowercase letter
+    """)
+    password = input("Password:\n")
+
+    password_length = len(password)
+    length_valid = True if password_length >= 5 else False
+    print(f"Password is {password_length} characters long, therefore length_valid is {length_valid}")
+    print()
+
+    uppercase_count = 0
+    for char in password:
+        if char.isupper():
+            uppercase_count =+ 1
+            break
+
+    lowercase_count = 0
+    for char in password:
+        if char.islower():
+            lowercase_count =+ 1
+            break
+
+    cases_valid = True if uppercase_count >= 1 and lowercase_count >= 1 else False
+    print(f"cases_valid is {cases_valid}")
 
 
 def create_account():
     """
     """
-    validate_username = validate_username_creation()
-    print("success")
-    # validate_password = validate_password_creation()
+    valid_username = validate_username_creation()
+    print(f"Username {valid_username} is available!\n")
+    valid_password = validate_password_creation()
+    print(f"Password valid!\n")
+    print("Finally, please tell us your name")
     # validate_name = validate_name_creation()
     
     # if validate_username and validate_password and validate_name:
